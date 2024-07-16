6142f27788b55c774cdfed1ec6687b322da79a8a
Babenko Liudmyla <savelieva0509@gmail.com>
2024-07-16 16:31:00 +0300

    add selector getFilteredTasks

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -1,34 +1,15 @@
 import { useSelector } from 'react-redux';
 import Task from '../Task/Task';
-import { getTasks, getFilter } from '.././../redux/selectors';
-import { statusFilters } from '.././../redux/constants';
-import { TasksState, FilterStatus } from '../../types';
+import { getFilteredTasks } from '../../redux/selectors';
+import { TaskTypes } from '../../types';
 import css from './TaskList.module.scss';
 
-const getVisibleTasks = (
-  tasks: TasksState['tasks'],
-  statusFilter: FilterStatus
-) => {
-  switch (statusFilter) {
-    case statusFilters.active: {
-      return tasks.filter(task => !task.completed);
-    }
-    case statusFilters.completed: {
-      return tasks.filter(task => task.completed);
-    }
-    default: {
-      return tasks;
-    }
-  }
-};
 const TaskList = () => {
-  const tasks = useSelector(getTasks);
-  const filter = useSelector(getFilter);
-  const visibleTasks = getVisibleTasks(tasks, filter);
+  const tasks = useSelector(getFilteredTasks) as TaskTypes[];
 
   return (
     <ul className={css.list}>
-      {visibleTasks.map(task => (
+      {tasks.map(task => (
         <li className={css.listItem} key={task.id}>
           <Task task={task} />
         </li>
--- src/redux/selectors.tsx
+++ src/redux/selectors.tsx
@@ -5,3 +5,19 @@ export const getTasks = (state: RootState) => state.tasks.tasks;
 export const getDeletedTasks = (state: RootState) => state.tasks.deletedTasks;
 
 export const getFilter = (state: RootState) => state.filter.status;
+
+export const getFilteredTasks = (state: RootState) => {
+  const { tasks, filter } = state;
+  switch (filter.status) {
+    case 'all':
+      return tasks.tasks; 
+    case 'active':
+      return tasks.tasks.filter(task => !task.completed);
+    case 'completed':
+      return tasks.tasks.filter(task => task.completed);
+    default:
+      return tasks.tasks;
+  }
+};
+
+
